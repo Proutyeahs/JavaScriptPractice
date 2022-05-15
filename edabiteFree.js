3dbcfd8dd4aa909dfb1dcc6e44c02c04a81b5466
Eric <103910650+Proutyeahs@users.noreply.github.com>
2022-05-15 17:28:10 -0500

    Create edabiteFree.js

---
+++ edabiteFree.js
@@ -0,0 +1,67 @@
+//Free trial did not last that long
+
+//Very Easy
+//1
+function hello() {
+	return "hello edabit.com"
+}
+//2
+function addition(a, b) {
+	return a + b
+}
+//3
+function convert(minutes) {
+	return minutes * 60
+}
+//4
+function addition(num) {
+	return num + 1
+}
+//5
+function triArea(base, height) {
+	return (base * height) / 2
+}
+//6
+function calcAge(age) {
+	return age * 365
+}
+//7
+function cubes(a) {
+	return a ** 3
+}
+//8
+function getFirstValue(arr) {
+	return arr[0]
+}
+//9
+function findPerimeter(length, width) {
+	return (length * 2) + (width * 2)
+}
+//10
+function howManySeconds(hours) {
+	return (hours * 60) * 60
+}
+//11
+function giveMeSomething(a) {
+	return "something" + " " + a
+}
+//12
+function remainder(x, y) {
+	return x % y
+}
+//13
+function nameString(name){
+	var b = "Edabit"
+	var result = name + b
+  	return result
+}
+//14
+function points(twoPointers, threePointers) {
+	return (twoPointers *2) + (threePointers * 3)
+}
+
+//Medium
+//1
+function countTrue(arr) {
+	return arr.filter(arr => arr === true).length;
+}
